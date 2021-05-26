3d30ac6c5711bb85a2c5c06a937cccb29bd3ea07
Joe <yaojx@connect.hku.hk>
2021-05-26 13:45:29 +0800

    tmp.

--- cell_based/test/Edited_Chaste/Qarameter1.hpp
+++ cell_based/test/Edited_Chaste/Qarameter1.hpp
@@ -76,7 +76,7 @@ public:
         // for sliding
 
         /*------------------------------START: Basic Settings----------------------------*/
-        double target_shape_index = 4.5;//p0
+        double target_shape_index = 0.75*4.5;//p0
         double reference_area = M_PI;
         double initial_area = reference_area;
         // bool   is_default_feedback_form = false;
@@ -86,7 +86,7 @@ public:
         double strip_width_mutiple = 8.0;
       //  double strip_width_multiple_for_sliding = 15.0;
         bool   if_use_larger_strip_distance = true; 
-        double strip_dis_multiplier = 60.0/12.0; // strip center distance, ensure the (num_ele_cross) being even number
+        double strip_dis_multiplier = 40.0/12.0; // strip center distance, ensure the (num_ele_cross) being even number
         bool   use_longer_mesh = true; // for (num_ele_up) mesh
         int    num_ele_up_multiplier = 2;
         int    move_mesh_right_for_N_periods = 0; // for display of multiple periods
@@ -103,7 +103,7 @@ public:
         double center_of_width = 0.0;       // change made by Chao
         double width = num_ele_cross*sqrt(initial_area/(sqrt(3)/2));   //width of reservoir, change made by Chao
 
-        double strip_width = 40*0.5*sqrt(initial_area/(sqrt(3)/2)); // default =0.9523 (1/2 cell width)
+        double strip_width = 20*0.5*sqrt(initial_area/(sqrt(3)/2)); // default =0.9523 (1/2 cell width)
         if  (strip_width_doubled_for_multiple_leading_cells)
             strip_width = strip_width*strip_width_mutiple;
       //  strip_width *= strip_width_multiple_for_sliding;
@@ -124,7 +124,7 @@ public:
         bool   use_fixed_target_area_without_modifier = true; // A0:
 
 /* 2. Myosin activity */
-        double nagai_honda_membrane_surface_energy_parameter = 0.1/(M_PI/reference_area);//Gamma
+        double nagai_honda_membrane_surface_energy_parameter = 0.75*0.2/(M_PI/reference_area);//Gamma
         double edge_length_at_rest = sqrt(initial_area/(6*sqrt(3)/4)); // = 1.0996
 
         bool   if_consider_feedback_of_element_myosin_activity = true;
@@ -241,7 +241,7 @@ public:
 
         // Cell polarity
         bool   has_polarity = true;
-        double polarity_magnitude = 0.3;
+        double polarity_magnitude = 0.2;
         bool   seed_manually = true;
         unsigned seed_for_initial_random_polarity = 1u;
         // seed_for_initial_random_polarity += 10;
